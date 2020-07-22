9088d17832e63a917cb22f7eda6824a5fcffbb03
yukiyr <37041284+yukiyr@users.noreply.github.com>
2020-07-22 09:25:53 +0700

    check if data exist in pendaftar table
    Jika pernah mendaftar sebelumnya, tidak akan bisa daftar lagi. Cek data double atau tidak di database

--- application/modules/pengajuan/models/Pendaftar_model.php
+++ application/modules/pengajuan/models/Pendaftar_model.php
@@ -18,11 +18,34 @@ class Pendaftar_model extends CI_Model
         return $this->db->get_where($this->_table, ["biodata_id" => $id])->result();
     }
 
+    public function pendaftar_exists($beasiswaid, $biodataid)
+    {
+        $this->db->where(['beasiswa_id'=>$beasiswaid, 'biodata_id'=>$biodataid]);
+        $query = $this->db->get('pendaftar');
+        if ($query->num_rows() > 0){
+            return 1;
+        }
+        else{
+            return 0;
+        }
+    }
+
     //insert ke tabel pendaftar
     public function save_pendaftar($beasiswaid, $biodataid)
     {
         $this->beasiswa_id = $beasiswaid;
         $this->biodata_id = $biodataid;
-        return $this->db->insert($this->_table, $this);
+        $status = $this->pendaftar_exists($beasiswaid, $biodataid); 
+        if( $status == 0)
+        {
+            return $this->db->insert($this->_table, $this);
+        }
+        else
+        {
+            echo "<script>
+            alert('Maaf Anda sudah terdaftar sebelumnya');
+            window.location='".site_url('pengajuan/periode')."';
+            </script>";
+        }
     }
 }
